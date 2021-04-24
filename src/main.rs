d9acf9696359970708860ca3351602a1f7dc6669
Bernardo Meurer <bernardo@meurer.org>
2021-04-23 18:38:52 -0700

    main: hand-roll gst pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,9 @@
-use anyhow::{anyhow, Error};
+use anyhow::{Context, Error};
 use gstreamer::prelude::*;
-use gstreamer_player::{PlayerGMainContextSignalDispatcher, PlayerSignalDispatcher};
 use structopt::StructOpt;
-use url::Url;
 
 use std::path::PathBuf;
-use std::sync::{Arc, Mutex};
-
-struct Player {
-    player: gstreamer_player::Player,
-    error: Arc<Mutex<Result<(), glib::Error>>>,
-}
-
-impl Player {
-    pub fn new(uri: &Url, main: &glib::MainLoop) -> Self {
-        let dispatcher = PlayerGMainContextSignalDispatcher::new(None);
-        let player = gstreamer_player::Player::new(
-            None,
-            Some(&dispatcher.upcast::<PlayerSignalDispatcher>()),
-        );
-
-        player.set_uri(uri.as_str());
-
-        let error = Arc::new(Mutex::new(Ok(())));
-
-        let main_clone = main.clone();
-        player.connect_end_of_stream(move |player| {
-            let main = &main_clone;
-            player.stop();
-            main.quit();
-        });
-
-        let main_clone = main.clone();
-        let error_clone = error.clone();
-        player.connect_error(move |player, err| {
-            let main = &main_clone;
-            let error = &error_clone;
-
-            *error.lock().expect("poisoned error mutex") = Err(err.clone());
-
-            player.stop();
-            main.quit();
-        });
-
-        Self { player, error }
-    }
-
-    pub fn play(&self) {
-        self.player.play();
-    }
-
-    pub fn as_error(&self) -> Result<(), glib::Error> {
-        self.error
-            .as_ref()
-            .lock()
-            .expect("poined error mutex")
-            .clone()
-            .map_err(Into::into)
-    }
-}
-
-fn gst_main(a: Url, b: Url) -> Result<(), Error> {
-    gstreamer::init()?;
-    let main = glib::MainLoop::new(None, false);
-
-    let a = Player::new(&a, &main);
-    let b = Player::new(&b, &main);
-
-    a.play();
-    b.play();
-
-    main.run();
-
-    a.as_error()?;
-    b.as_error()?;
-
-    Ok(())
-}
+use std::sync::atomic::AtomicUsize;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "abx", about = "CLI utility to ABX audio files.")]
@@ -85,9 +12,96 @@ struct Opt {
     b: PathBuf,
 }
 
+struct AudioStreamSelector {
+    selection: AtomicUsize,
+}
+
 fn main() -> Result<(), Error> {
     let opt = Opt::from_args();
-    let a = Url::from_file_path(opt.a).map_err(|_| anyhow!("Failed to convert path A to URI"))?;
-    let b = Url::from_file_path(opt.b).map_err(|_| anyhow!("Failed to convert path A to URI"))?;
-    gst_main(a, b)
+
+    gstreamer::init()?;
+
+    let pipeline = gstreamer::Pipeline::new(Some("abx"));
+
+    let a_src = gstreamer::ElementFactory::make("filesrc", None)?;
+    a_src.set_property("location", &opt.a.as_os_str().to_str())?;
+    let b_src = gstreamer::ElementFactory::make("filesrc", None)?;
+    b_src.set_property("location", &opt.b.as_os_str().to_str())?;
+
+    let a_decoder = gstreamer::ElementFactory::make("decodebin", None)?;
+    let b_decoder = gstreamer::ElementFactory::make("decodebin", None)?;
+
+    let mixer = gstreamer::ElementFactory::make("audiomixer", Some("mixer"))?;
+
+    let audiosink = gstreamer::ElementFactory::make("autoaudiosink", None)?;
+
+    pipeline.add_many(&[&a_src, &a_decoder, &b_src, &b_decoder, &mixer, &audiosink])?;
+
+    a_src
+        .link(&a_decoder)
+        .with_context(|| "Failed to link A to decoder")?;
+
+    let a_mixer_pad = mixer
+        .request_pad(
+            &mixer
+                .get_pad_template("sink_%u")
+                .expect("Failed to get sink template for mixer"),
+            None,
+            None,
+        )
+        .expect("Failed to get a_mixer_pad");
+
+    // a_mixer_pad.set_property("mute", &true)?;
+
+    a_decoder.connect_pad_added(move |_, pad| {
+        pad.link(&a_mixer_pad).unwrap();
+    });
+
+    b_src
+        .link(&b_decoder)
+        .with_context(|| "Failed to link B to decoder")?;
+
+    let b_mixer_pad = mixer
+        .request_pad(
+            &mixer
+                .get_pad_template("sink_%u")
+                .expect("Failed to get sink template for mixer"),
+            None,
+            None,
+        )
+        .expect("Failed to get b_mixer_pad");
+
+    b_mixer_pad.set_property("mute", &true)?;
+
+    b_decoder.connect_pad_added(move |_, pad| {
+        pad.link(&b_mixer_pad).unwrap();
+    });
+
+    mixer
+        .link(&audiosink)
+        .with_context(|| "Failed to link mixer to audio sink")?;
+
+    pipeline.set_state(gstreamer::State::Playing)?;
+
+    let bus = pipeline.get_bus().expect("FIXME");
+    loop {
+        let message = bus.timed_pop(gstreamer::ClockTime::none());
+        if let Some(msg) = message {
+            use gstreamer::MessageView::*;
+            match msg.view() {
+                Eos(_) => {
+                    pipeline.set_state(gstreamer::State::Paused)?;
+                    break;
+                }
+                Error(e) => {
+                    eprintln!("{:?}", e);
+                }
+                Progress(p) => {
+                    println!("{:?}", p);
+                }
+                _ => continue,
+            }
+        }
+    }
+    Ok(())
 }
